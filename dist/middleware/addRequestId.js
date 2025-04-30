c251cce1898a790f422bc728fce978a40666db95
Ethan Strauss <estrauss@gmail.com>
2025-04-30 14:39:58 -0300

    remove dist file

--- dist/middleware/addRequestId.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addRequestId = void 0;
-const uuid_1 = require("uuid");
-const addRequestId = (req, _res, next) => {
-    req.requestId = (0, uuid_1.v4)();
-    next();
-};
-exports.addRequestId = addRequestId;
